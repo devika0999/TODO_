13b90eba0dd9b7a31f1939506f5a18bec5c6742a
devika0999 <krishnadevika445@gmail.com>
2022-10-12 20:11:15 +0530

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TodoComponent } from './todo/todo.component';
-
+import { HttpClientModule} from '@angular/common/http';
+const appRoutes:Routes=[
+  {
+    path:"",component:TodoComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +20,9 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { 
+this.fetchData()
+  }
 
+  fetchData=()=>{
+
+    this.myapi.viewData().subscribe(
+      (data)=>{
+        this.todoData=data
+      }
+    )
+  }
+
+  todoData:any={}
   ngOnInit(): void {
   }
 
